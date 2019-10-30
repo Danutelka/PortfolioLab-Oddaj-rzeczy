025b148a704d1c35b0d34def0658737cb014b7c4
Danuta <kawecka.d@gmail.com>
2019-10-30 21:09:38 +0100

    nowosci

--- static/js/form.js
+++ static/js/form.js
@@ -32,3 +32,42 @@ document.addEventListener("DOMContentLoaded", function() {
         tr9.innerText = uwagi.value;
     })
 });
+
+
+<script>
+function Podsumowanie() {
+        //var kategoria = document.getElementById("id_categories").value;
+        var ilosc = document.getElementById("id_quantity").value;
+        var instytucja = document.getElementById("id_institution").value;
+        var adres = document.getElementById("id_adress").value;
+        var miasto = document.getElementById("id_city").value;
+        var kod = document.getElementById("id_zip_code").value;
+        var telefon = document.getElementById("id_phone_number").value;
+        var data_odbior = document.getElementById("id_pick_up_date").value;
+        var czas_odbior = document.getElementById("id_pick_up_time").value;
+        var komentarz = document.getElementById("id_pick_up_comment").value;
+        document.getElementById('worki').innerHTML = ilosc+" worki";
+
+        var ilosc = document.getElementById('ilosc');
+        var org = document.getElementById('org');
+        var adres = document.getElementById('adres');
+        var miasto = document.getElementById('miasto');
+        var kod = document.getElementById('kod');
+        var tel = document.getElementById('tel');
+        var data = document.getElementById('data');
+        var godz = document.getElementById('godz');
+        var uwagi = document.getElementById('uwagi');
+        var gotowe = document.getElementById('gotowe');
+
+
+
+        document.getElementById('dla-kogo').innerHTML = "Dla "+instytucja;
+        document.getElementById('sum_adres').innerHTML = adres;
+        document.getElementById('sum_miasto').innerHTML = miasto;
+        document.getElementById('sum_kod').innerHTML = kod;
+        document.getElementById('sum_tel').innerHTML = telefon;
+        document.getElementById('data-odbioru').innerHTML = data_odbior;
+        document.getElementById('czas-odbioru').innerHTML = czas_odbior;
+        document.getElementById('uwagi').innerHTML = komentarz;
+      }
+</script>
